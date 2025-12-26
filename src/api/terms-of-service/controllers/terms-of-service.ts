66883cbd5d59a966417faf650a03eed665356977
redaezziani <klausdev2@gmail.com>
2025-12-26 11:02:59 +0100

    fix: add TypeScript ignore comments for generated types in terms-of-service controller, router, and service

--- src/api/terms-of-service/controllers/terms-of-service.ts
+++ src/api/terms-of-service/controllers/terms-of-service.ts
@@ -4,4 +4,5 @@
 
 import { factories } from '@strapi/strapi';
 
+// @ts-ignore - Type will be generated after first build
 export default factories.createCoreController('api::terms-of-service.terms-of-service');
--- src/api/terms-of-service/routes/terms-of-service.ts
+++ src/api/terms-of-service/routes/terms-of-service.ts
@@ -4,4 +4,5 @@
 
 import { factories } from '@strapi/strapi';
 
+// @ts-ignore - Type will be generated after first build
 export default factories.createCoreRouter('api::terms-of-service.terms-of-service');
